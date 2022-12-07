6b707bb73a77cff202d1ade736e65b57ef81861a
Sergey Iryupin <biblelamp@gmail.com>
2022-12-07 20:39:20 +0100

    update #1

---
+++ lesson36/src/main/java/spring/controller/RestSimpleController.java
@@ -0,0 +1,26 @@
+package spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import spring.domain.Person;
+import spring.service.PersonService;
+
+@RestController
+@RequestMapping("/api")
+public class RestSimpleController implements GetApi {
+
+    @Autowired
+    PersonService personService;
+
+    public ResponseEntity<Person> get(@PathVariable("id") Integer id) {
+        Person person = personService.findById(id);
+        if (person != null) {
+            return ResponseEntity.ok(person);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
